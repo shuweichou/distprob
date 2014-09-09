7d71b3f15a8abbf8c354b47f08b6e3a2aa9d3d15
shuweichou <vicchou325@gmail.com>
2014-09-09 12:39:23 -0600

    Create server.R

---
+++ server.R
@@ -0,0 +1,12 @@
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+    output$binomplot<-renderPlot({
+      x<-seq(0,input$n,1)
+      p<-dbinom(x, input$n, input$p)
+      plot(x,p,type="h",xlim=c(-1,(input$n)+1),ylim=c(0,1),lwd=2,col="blue",ylab="p")  
+      points(x,p,pch=16,cex=2,col="dark red") 
+    })  
+})
